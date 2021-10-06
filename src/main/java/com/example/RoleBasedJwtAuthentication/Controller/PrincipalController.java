3fb3e6849dd0a55cc8cabe51a3f6b4ce08dd3c41
Your Name <khushi.jiyani1@gmail.com>
2021-10-06 17:45:23 +0530

    PreAuthorization Done for PrincipalController

--- src/main/java/com/example/RoleBasedJwtAuthentication/Controller/PrincipalController.java
+++ src/main/java/com/example/RoleBasedJwtAuthentication/Controller/PrincipalController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,18 +20,21 @@ public class PrincipalController {
     private final PrincipalService principalService;
 
     @PostMapping("/addPrincipal")
+    @PreAuthorize("hasRole('Principal')")
     public ResponseEntity<?> addPrincipal(@RequestBody PrincipalDto principalDto){
         PrincipalDto dto = principalService.addPrincipal(principalDto);
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
 
     @GetMapping("/getPrincipalByPrincipalId/{principalId}")
+    @PreAuthorize("hasRole('Principal')")
     public ResponseEntity<?> getPrincipalByPrincipalId(@PathVariable String principalId){
         PrincipalDto dto = principalService.getPrincipalByPrincipalId(principalId);
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @GetMapping("/getAllPrincipals/{pageNo}")
+    @PreAuthorize("hasAnyRole('Teacher','Principal','student')")
     public ResponseEntity<?> getAllPrincipals(@PathVariable(value = "pageNo") int pageNo){
         Page<PrincipalDto> page = principalService.getAllPrincipals(pageNo);
         List<PrincipalDto> collegeDtoList = page.getContent();
