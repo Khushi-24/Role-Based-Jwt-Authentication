0f512f4cc7d8d8fe7e9bd1a409c7643b897e5001
Your Name <khushi.jiyani1@gmail.com>
2021-10-06 17:51:04 +0530

    PreAuthorization Done for StudentController

--- src/main/java/com/example/RoleBasedJwtAuthentication/Controller/StudentController.java
+++ src/main/java/com/example/RoleBasedJwtAuthentication/Controller/StudentController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,18 +20,21 @@ public class StudentController {
     private final StudentService studentService;
 
     @PostMapping("/addStudent")
+    @PreAuthorize("hasAnyRole('Teacher','Principal','student')")
     public ResponseEntity<?> addStudent(@RequestBody StudentDto studentDto){
         StudentDto dto = studentService.addStudent(studentDto);
         return  new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
 
     @GetMapping("/getStudentByStudentId/{studentId}")
+    @PreAuthorize("hasAnyRole('Teacher','Principal','student')")
     public ResponseEntity<?> getStudentByStudentId(@PathVariable String studentId){
         StudentDto dto = studentService.getStudentByStudentId(studentId);
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @GetMapping("/getAllStudents/{pageNo}")
+    @PreAuthorize("hasAnyRole('Teacher','Principal')")
     public ResponseEntity<?> getAllStudents(@PathVariable(value = "pageNo") int pageNo){
         Page<StudentDto> page = studentService.getAllStudents(pageNo);
         List<StudentDto> dto = page.getContent();
@@ -41,12 +45,14 @@ public class StudentController {
     }
 
     @GetMapping("/getStudentHavingCpiGreaterThan/{cpi}")
+    @PreAuthorize("hasAnyRole('Teacher','Principal')")
     public ResponseEntity<?> getStudentHavingCpiGreaterThan(@PathVariable float cpi){
         int count= studentService.getStudentHavingCpiGreaterThan(cpi);
         return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
     @GetMapping("/getStudentHavingCpiLessThan/{cpi}")
+    @PreAuthorize("hasAnyRole('Teacher','Principal')")
     public ResponseEntity<?> getStudentHavingCpiLessThan(@PathVariable float cpi){
         int count= studentService.getStudentHavingCpiLessThan(cpi);
         return new ResponseEntity<>(count, HttpStatus.OK);
