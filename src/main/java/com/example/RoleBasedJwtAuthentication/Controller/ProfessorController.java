716eba03163702d00d847d995f7f77daf26119be
Your Name <khushi.jiyani1@gmail.com>
2021-10-06 17:48:05 +0530

    PreAuthorization Done for ProfessorController

--- src/main/java/com/example/RoleBasedJwtAuthentication/Controller/ProfessorController.java
+++ src/main/java/com/example/RoleBasedJwtAuthentication/Controller/ProfessorController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,24 +19,28 @@ public class ProfessorController {
     private final ProfessorService professorService;
 
     @PostMapping("/addProfessor")
+    @PreAuthorize("hasAnyRole('Teacher','Principal')")
     public ResponseEntity<?> addProfessor(@RequestBody ProfessorDto professorDto){
         ProfessorDto dto = professorService.addProfessor(professorDto);
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
 
     @PostMapping("/addProfessorToDepartment")
+    @PreAuthorize("hasRole('Principal')")
     public ResponseEntity<?> addProfessorToDepartment(@RequestBody ProfessorDepartmentDto professorDepartmentDto){
         professorService.addProfessorToDepartment(professorDepartmentDto);
         return ResponseEntity.ok("Professor Added to department successfully.");
     }
 
     @GetMapping("/getProfessorById/{professorId}")
+    @PreAuthorize("hasAnyRole('Teacher','Principal')")
     public ResponseEntity<?> getProfessorById(@PathVariable String professorId){
         ProfessorDto dto = professorService.getProfessorById(professorId);
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     @GetMapping("/getAllProfessor/{pageNo}")
+    @PreAuthorize("hasAnyRole('Teacher','Principal','student')")
     public ResponseEntity<?> getAllProfessor(@PathVariable(value = "pageNo") int pageNo){
         Page<ProfessorDto> page = professorService.getAllProfessor(pageNo);
         List<ProfessorDto> professorDto = page.getContent();
